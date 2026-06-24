2feca394563ddbf53214fc6d7bbea7ef93010a87
suho365-ahn <ahndaebok@gmail.com>
2026-06-24 13:15:53 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,20 +1,14 @@
-const CACHE = 'jubbo-v1';
-const ASSETS = ['./index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
+const CACHE = 'jubo-v1';
+const ASSETS = ['./index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
 });
-
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
   self.clients.claim();
 });
-
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('./index.html')))
-  );
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
