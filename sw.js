da17b1356b258543850fe05fe009a449a55752b2
suho365-ahn <ahndaebok@gmail.com>
2026-07-05 09:56:43 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,14 +1,28 @@
-const CACHE = 'jubo-v1';
+const CACHE = 'jubo-v2';
 const ASSETS = ['./index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
 });
+
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  e.waitUntil(
+    caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k))))
+  );
   self.clients.claim();
 });
+
+// 네트워크 우선 → 실패(오프라인)하면 캐시 사용
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        // 성공적으로 받아온 응답은 캐시에도 최신 버전으로 갱신
+        const resClone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, resClone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
 });
